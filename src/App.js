da28d1e92c2ab8d10e789f706e870548af5fa9e6
supriya-5 <supriyaprabu005@gmail.com>
2022-07-13 15:20:28 +0530

    day6

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import Memo from './day4/Memo';
 import Reference from './day4/Reference';
 
 
+
 function App() {
   return (
     <div className="App">
       <Reducer/>
       <Memo/>
       <Reference/>
+
+
     </div>
   );
 }
